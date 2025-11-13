a011be55854d7f2d7b7e67ffbce9a53ee9dee1b1
shivangamin-art <shivang.amin@ontariotechu.net>
2025-11-13 00:30:10 -0500

    added 4 courses to the course model and updated app component to display them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { COURSES } from '../db-data';
+import { Course } from '../model/course';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: false
 })
 export class AppComponent {
 
+  title = 'angular-course';  // fixes unit test
 
+  // Load the first 4 courses
+  courses: Course[] = COURSES.slice(0, 4);
 
 }
