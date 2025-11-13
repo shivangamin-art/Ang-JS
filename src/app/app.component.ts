a854c3ed41d1e6744199f25586acef2235e5b863
shivangamin-art <shivang.amin@ontariotechu.net>
2025-11-12 21:26:59 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {COURSES} from '../db-data';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: false
+})
+export class AppComponent {
+
+
+
+}
